e60bcb9f8fec101dbea09d50e981fcdff46dad13
Ron Nazarov <ron@noisytoot.org>
2024-06-02 03:50:39 +0100

    Reindent util/make-circle-data.lisp with spaces

--- util/make-circle-data.lisp
+++ util/make-circle-data.lisp
@@ -1,3 +1,5 @@
+;;;; -*- mode: lisp; indent-tabs-mode: nil -*-
+
 ;;;; SPDX-FileCopyrightText: 2024 Ron Nazarov
 ;;;; SPDX-License-Identifier: NGPL OR GPL-2.0-or-later
 
@@ -13,22 +15,22 @@
 
 (defun make-circle-data (max-radius)
   (loop for max-radius from 1 to max-radius
-	collect (loop for current-radius from 0 to max-radius
-		      collect (circle-limit-offset max-radius current-radius))))
+        collect (loop for current-radius from 0 to max-radius
+                      collect (circle-limit-offset max-radius current-radius))))
 
 (defun print-circle-data (data)
   (let ((absolute-index 0)
-	(max-radius (length (car (last data)))))
+        (max-radius (length (car (last data)))))
     (format t "char circle_data[] = {~%")
     (loop for line in data
-	  do (loop for relative-index from 0
-		   for num in line
-		   do (if (= relative-index 0)
-			  (format t "/*~3d*/~2d," absolute-index num)
-			  (format t "~d," num))
-		      (incf absolute-index))
-	     (terpri))
+          do (loop for relative-index from 0
+                   for num in line
+                   do (if (= relative-index 0)
+                          (format t "/*~3d*/~2d," absolute-index num)
+                          (format t "~d," num))
+                      (incf absolute-index))
+             (terpri))
     (format t "/*~3d*/ ~d /* should be MAX_RADIUS+1; used to terminate range loops -dlc */~%};~%"
-	    absolute-index max-radius)))
+            absolute-index max-radius)))
 
 (print-circle-data (make-circle-data 32))
